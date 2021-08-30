cfb3314eb4db14c49dfd30e34c69077e732e6613
akicho8 <akicho8@gmail.com>
2021-08-30 14:27:15 +0900

    [feat] annotate, motion_blur, matrix

---
+++ 0140_annotate.rb
@@ -0,0 +1,21 @@
+require "./setup"
+
+canvas = Magick::Image.read("granite:").first
+canvas.resize!(200, 200)
+
+char_w = 100
+
+gc = Magick::Draw.new
+gc.annotate(canvas, char_w - 1, char_w - 1, 0, 0, "龍") do |e|
+  e.font      = "/System/Library/Fonts/ヒラギノ明朝 ProN.ttc"
+  e.fill      = "black"
+  e.pointsize = char_w / 2
+  e.gravity   = Magick::CenterGravity
+end
+
+gc.fill = "rgba(0,0,0,0.5)"
+gc.rectangle(0, 0, char_w - 1, char_w - 1)
+gc.draw(canvas)
+
+canvas.write("_annotate.png")
+`open _annotate.png`
