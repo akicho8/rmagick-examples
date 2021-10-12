d2c2c864e5dd1ffeb3c95bd047921fb2e27ff36a
akicho8 <akicho8@gmail.com>
2021-10-12 19:30:07 +0900

    [feat] プラズマグラデーション更新

--- 0120_bioshogi_背景_グラデーション_プラズマ.rb
+++ 0120_bioshogi_背景_グラデーション_プラズマ.rb
@@ -6,13 +6,15 @@ end
 
 COUNT = 4
 COUNT.times do |i|
-  c = Color::HSL.from_fraction(1.0.fdiv(COUNT) * i, 0.7, 0.7)
+  c = Color::HSL.from_fraction(1.0.fdiv(COUNT) * i, 0.7, 0.8)
   c1 = c.dup.tap { |e| e.hue += 0      }
   c2 = c.dup.tap { |e| e.luminosity    }
   layer = Image.read("plasma:#{c1.html}-#{c2.html}") { |e| e.size = "1920x1080" }.first
+  # layer = layer.swirl(180*1)
+  # layer = layer.implode(1.0)
   layer = layer.blur_image(0, 50)
-  # layer = layer.swirl(180*2)
   # layer = layer.wave(25/2, 150)
   d layer
+  # exit
   layer.write(fn(i.next))
 end
