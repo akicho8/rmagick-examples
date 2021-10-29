5bc142d27f82ea4d9620256605d84c03f4a3695a
akicho8 <akicho8@gmail.com>
2021-10-29 16:25:19 +0900

    [feat] 右下に文章配置

--- 0262_draw_annotate_円の上に数字をレイヤーの左上に配置.rb
+++ 0262_draw_annotate_円の上に数字をレイヤーの左上に配置.rb
@@ -1,29 +1,21 @@
 require "./setup"
 
-layer1 = Image.new(800, 800) { |e| e.background_color = "white" }
+layer1 = Image.new(1920, 1080) { |e| e.background_color = "white" }
 
-str = "123"
-pointsize = 40
-w = 98
-h = w
-x = 0
-y = 0
-r = 42
+str = "BGM: SONG-NAME written by author-name"
+pointsize = 32
+w = layer1.columns
+h = pointsize
+margin_x = 8
+margin_y = 6
 layer2 = Image.new(w, h) { |e| e.background_color = "transparent" }
 
 gc = Draw.new
-gc.fill("hsla(0,0%,0%,0.9)")
-from = [x + w/2, y + h/2]
-to   = [x + w/2 + r, y + h/2]
-gc.circle(*from, *to)
-gc.draw(layer2)
-
-gc = Draw.new
-gc.annotate(layer2, w, h, x, y, str) do |e|
+gc.annotate(layer2, w, h, 0, 0, str) do |e|
   e.font      = "fonts/RictyDiminished-Bold.ttf"
-  e.fill      = "hsla(0,0%,100%,0.9)"
+  e.fill      = "hsla(0,0%,0%,1.0)"
   e.pointsize = pointsize
-  e.gravity   = CenterGravity
+  e.gravity   = SouthEastGravity
 end
 
-d layer1.composite(layer2, 0, 0, OverCompositeOp)
+d layer1.composite(layer2, SouthEastGravity, margin_x, margin_y, OverCompositeOp)
