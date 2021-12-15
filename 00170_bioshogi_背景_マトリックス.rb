57ec09d81adcf2d094217ba15889e5f52627cc00
akicho8 <akicho8@gmail.com>
2021-12-15 11:46:37 +0900

    [feat] twitter_banner_matrix/* 生成

--- 00170_bioshogi_背景_マトリックス.rb
+++ 00170_bioshogi_背景_マトリックス.rb
@@ -12,10 +12,11 @@ class App
       @x = x.to_f
       @y = rand(base.yn).to_f
       @index = rand(base.char_list.size)
+      @speed = rand(1.0..1.5)
     end
 
     def update
-      @y += 1
+      @y += @speed
       if @y >= base.yn
         @y -= base.yn
       end
@@ -24,10 +25,12 @@ class App
 
     def draw
       gc = Draw.new
-      gc.annotate(base.canvas, base.char_w - 1, base.char_h - 1, x * base.char_w, y * base.char_h, char) do |e|
+      rx = x * base.char_w
+      ry = y * base.char_h
+      gc.annotate(base.canvas, base.char_w - 1, base.char_h - 1, rx, ry, char) do |e|
         e.font      = base.font_file.to_s
         e.fill      = base.main_color
-        e.pointsize = base.char_w
+        e.pointsize = base.char_w + swing
         e.gravity   = CenterGravity
       end
     end
@@ -35,30 +38,23 @@ class App
     def char
       base.char_list[@index.modulo(base.char_list.size)]
     end
+
+    def swing
+      a = base.char_w * 0.1
+      b = base.char_w * 0.9
+      rand(-a..b)
+    end
   end
 
-  attr_accessor :char_list
   attr_accessor :canvas
-  attr_accessor :char_w
-  attr_accessor :char_h
-  attr_accessor :font_file
-  attr_accessor :main_color
   attr_accessor :params
 
   def initialize(params = {})
     @params = params
 
-    @char_w = 10
-    @char_h = 10
-
-    @generations = yn
-
-    @dark_level = 0.16
-
-    @font_file = Pathname("fonts/RictyDiminished-Bold.ttf").expand_path
-    @main_color = "rgb(0,255,0)"
-
-    @char_list = Bioshogi::TacticInfo.all_elements.collect(&:name).shuffle.join.chars.reverse
+    if seed
+      srand(seed)
+    end
   end
 
   def run
@@ -66,35 +62,46 @@ class App
 
     @points = xn.times.collect { |x| Ship.new(self, x) }
 
-    @generations.times do
+    yn.times do
       @points.each do |point|
         point.update
         point.draw
       end
       gc = Draw.new
-      gc.fill = "rgba(0,0,0,#{@dark_level})"
+      gc.fill = "rgba(0,0,0,#{dark_level})"
       gc.rectangle(0, 0, @canvas.columns - 1, @canvas.rows - 1)
       gc.draw(@canvas)
     end
 
     if true
-      effect = @canvas.copy
+      original = @canvas.copy
+
+      effect = original.copy
       effect.colorspace = GRAYColorspace
-      effect = effect.spread(6).blur_image(0, 2)
-      effect.write("PNG24:_matrix_effect.png")
+      effect = effect.spread(6)
+      effect = effect.blur_image(0, 2)
+      # effect = effect.motion_blur(0, 10, 90)
+      effect.write("PNG24:_matrix_effect1.png")
       @canvas = @canvas.composite(effect, 0, 0, LinearDodgeCompositeOp)
+
+      effect = original.copy
+      # effect.colorspace = GRAYColorspace
+      effect = effect.motion_blur(0, 20, 90)
+      effect.write("PNG24:_matrix_effect2.png")
+      @canvas = @canvas.composite(effect, 0, 0, ScreenCompositeOp)
     end
 
+    FileUtils.makedirs(output.dirname)
     @canvas.write("PNG24:#{output}")
-    `open #{output}`
+    # `open #{output}`
   end
 
   def yn
-    @yn ||= image_h.fdiv(@char_h).ceil
+    @yn ||= image_h.fdiv(char_h).ceil
   end
 
   def xn
-    @xn ||= image_w.fdiv(@char_w).ceil
+    @xn ||= image_w.fdiv(char_w).ceil
   end
 
   def image_w
@@ -105,9 +112,51 @@ class App
     params[:image_h] || 1080
   end
 
+  def char_w
+    params[:char_w] || 14
+  end
+
+  def char_h
+    params[:char_h] || 14
+  end
+
+  def main_color
+    params[:main_color] || "hsla(#{main_hue},100%,50%,1.0)"
+  end
+
+  def main_hue
+    params[:main_hue] || 120 + 20
+  end
+
+  def dark_level
+    params[:dark_level] || 0.16 + 0.1
+  end
+
+  def seed
+    params[:seed]
+  end
+
+  def char_list
+    @char_list ||= -> {
+      s = params[:char_list] || Bioshogi::TacticInfo.all_elements.collect(&:name).shuffle.join
+      s = s.tr("ー→", "|↓")
+      s.chars
+    }.call
+  end
+
+  def font_file
+    @font_file ||= Pathname("fonts/RictyDiminished-Bold.ttf").expand_path
+  end
+
   def output
-    params[:output] || "_matrix.png"
+    Pathname(params[:output] || "_matrix.png").expand_path
   end
 end
 
-App.new(output: "../bioshogi/lib/bioshogi/assets/images/background/matrix.png").run
+App.new(output: "../bioshogi/lib/bioshogi/assets/images/background/matrix.png", char_w: 20, char_h: 20, main_hue: 140).run
+
+App.new(output: "twitter_banner_matrix1500x500.png", image_w: 640, image_h: 480, char_w: 16, char_h: 16).run
+
+(1..100).each do |i|
+  App.new(output: "twitter_banner_matrix/#{i}.png", image_w: 1500, image_h: 500, char_w: 14, char_h: 14, seed: i).run
+end
--- 00540_motion_blur.rb
+++ 00540_motion_blur.rb
@@ -1,3 +1,3 @@
 require "./setup"
 image = Image.read("logo:").first
-d image.motion_blur(0, 10, 90 * 3) # radius, sigma, angle
+d image.motion_blur(0, 10, 90) # radius, sigma, angle
